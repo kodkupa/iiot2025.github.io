d2e712431183eec8a62a90a7e9c8eb89ff44f935
TkcsHnr <tkcshnr@gmail.com>
2025-04-06 21:15:51 +0200

    schedule page

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -2,7 +2,7 @@
 	import Hero from '$lib/components/page/Hero.svelte';
 	import Content from '$lib/components/page/Content.svelte';
 	import Date from './Date.svelte';
-	import Description from '$lib/components/page/Description.svelte';
+	import { base } from '$app/paths';
 </script>
 
 <svelte:head>
@@ -12,41 +12,32 @@
 <Hero>Schedule</Hero>
 
 <Content>
-	<Description>
-		<p>The International final of IIOT 2024 will be held as a hybrid (onsite and online) contest from May 10th to 13th in Damascus, Syria. Detailed information and the schedule of those days can be found on the <a class="btn-link link-secondary" href="https://www.iiot2024.sy">website</a> of the final.</p>
-		<p>Below is the schedule for the Italian, Romanian and Hungarian qualifiers. Each regular member can
-		have its <span class="font-bold">own schedule</span> for their national competition.</p>
-	</Description>
-	<div class="max-w-xl w-full flex flex-col text-center gap-4">
-		<Date>
-			<h2 slot="title">1st qualifying round</h2>
-			<p slot="date">13 November 2023</p>
-		</Date>
-		<Date>
-			<h2 slot="title">2nd qualifying round</h2>
-			<p slot="date">12 December 2023</p>
-		</Date>
-		<Date>
-			<h2 slot="title">3rd qualifying round</h2>
-			<p slot="date">17 January 2024</p>
-		</Date>
-		<Date>
-			<h2 slot="title">4th qualifying round</h2>
-			<p slot="date">8 February 2024</p>
-		</Date>
-		<Date>
-			<h2 slot="title">National finals</h2>
-			<p slot="date">March, 2024</p>
-		</Date>
-		<Date>
-			<h2 slot="title">International finals</h2>
-			<p slot="date">10-13 May, 2024</p>
-		</Date>
-	</div>
+	<p class="mx-auto">
+		Deadline for <a href="{base}/registration" class="btn-link link-secondary">registration</a> of teams
+		and delegation members is April 30.
+	</p>
+	<h2 class="text-2xl sm:divider">Schedule for contest days</h2>
+	<p>A detailed schedule will be published at least two weeks before the contest.</p>
 </Content>
-
-<style>
-	h2 {
-		font-weight: bold;
-	}
-</style>
+<div class="max-w-xl w-full flex flex-col text-center gap-4">
+	<Date>
+		<h2 slot="title">May 30 (Friday)</h2>
+		<p slot="date">Arrivals @Hotel (from 15:00). Dinner will be provided.</p>
+	</Date>
+	<Date>
+		<h2 slot="title">May 31 (Saturday)</h2>
+		<p slot="date">Opening Ceremony @Fazekas & Practice contest @Fazekas</p>
+	</Date>
+	<Date>
+		<h2 slot="title">June 1 (Sunday)</h2>
+		<p slot="date">Contest @Fazekas</p>
+	</Date>
+	<Date>
+		<h2 slot="title">June 2 (Monday)</h2>
+		<p slot="date">Sightseeing @Budapest & Closing Ceremony @Fazekas</p>
+	</Date>
+	<Date>
+		<h2 slot="title">June 3 (Tuesday)</h2>
+		<p slot="date">Departures @Hotel (until 12:00, breakfast will be provided)</p>
+	</Date>
+</div>
--- src/routes/schedule/Date.svelte
+++ src/routes/schedule/Date.svelte
@@ -1,17 +1,17 @@
 <script>
-    import Calendar from "./Calendar.svelte";
+	import Calendar from './Calendar.svelte';
 </script>
 
 <div class="card card-compact flex flex-col bg-base-200 shadow-md">
-    <div class="card-body gap-2 sm:gap-0">
-        <div class="text-xl font-bold sm:divider mt-0 sm:mt-2 h-auto">
-            <slot name="title"></slot>
-        </div>
-        <div class="flex justify-center gap-3">
-            <Calendar />
-            <div class="text-lg grow-0">
-                <slot name="date"></slot>
-            </div>
-        </div>
-    </div>
+	<div class="card-body gap-2 sm:gap-0">
+		<div class="text-xl font-bold sm:divider mt-0 sm:mt-2 h-auto">
+			<span><Calendar /></span>
+			<slot name="title" />
+		</div>
+		<div class="flex justify-center gap-3">
+			<div class="text-lg grow-0">
+				<slot name="date" />
+			</div>
+		</div>
+	</div>
 </div>
