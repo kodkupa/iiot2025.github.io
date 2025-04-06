d32e6f228ba129f2f85389ff91f683ca19a15537
TkcsHnr <tkcshnr@gmail.com>
2025-04-06 18:49:47 +0200

    landing page content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,9 @@
 	<title>Home</title>
 </svelte:head>
 
-<div class="w-full flex justify-center bg-base-200 border-b-[1.5px] border-base-300 shadow-sm z-10 py-6 lg:py-12 px-6">
+<div
+	class="w-full flex justify-center bg-base-200 border-b-[1.5px] border-base-300 shadow-sm z-10 py-6 lg:py-12 px-6"
+>
 	<div class="max-w-3xl w-full">
 		<Logo />
 	</div>
@@ -16,62 +18,46 @@
 
 <Content>
 	<div class="w-full max-w-4xl text-lg flex flex-col px-2">
+		<h2 class="text-2xl sm:divider">IIOT International Final 2025 - Budapest, Hungary</h2>
+		<div class="flex flex-col gap-2 text-justify">
+			<p>
+				The <a href="https://iio.team" class="btn-link link-secondary"
+					>International Informatics Olympiad in Teams</a
+				>
+				(IIOT) is a prestigious computer programming competition for teams of high school students. The
+				participants are teams of four high school students representing their schools. The IIOT 2025
+				International finals take place in Budapest from May 30 to June 2.
+			</p>
+			<p>
+				The seven regular member countries (Bulgaria, Egypt, Hungary, Italy, Romania, Syria, USA)
+				participate with at most 3 teams, and also <b
+					>guest teams from any country are invited to take part in the competition onsite or online</b
+				>. See the <a href="{base}/registration" class="btn-link link-secondary">registration</a> page
+				for more details.
+			</p>
+		</div>
 		<h2 class="text-2xl sm:divider">About IIOT</h2>
 		<div class="flex flex-col gap-2 text-justify">
 			<p>
-				The IIOT is a young project which aims to be just like the
-				<a href="https://ioinformatics.org/" class="btn-link link-secondary">International Olympiad in Informatics</a>,
-				but the problems are meant to be solved in a team.
+				IIOT started in Italy and has been an international competition since 2017. It aims to fill
+				an important gap, as there was previously no prestigious programming team competition for
+				high school students comparable to the ICPC university competition. However, the problems
+				are similar to tasks in the International Olympiad in Informatics (IOI).
 			</p>
 			<p>
-				The participants are teams of 4 high school students who represent a school (like in
-				<a href="https://icpc.global/" class="btn-link link-secondary">ICPC</a>).
+				The competition emphasizes teamwork, creativity, and collaborative learning — skills
+				essential in the modern workforce. It includes four online rounds, a national final, and an
+				international onsite contest. Each country is coordinated by a leader school or organization
+				with support from the international scientific committee. The number of participating
+				countries is constantly increasing. To get involved, feel free to reach out to
+				<a href="mailto:iio.team@iio.team" class="btn-link link-secondary">iio.team@iio.team</a>.
 			</p>
-		</div>
-		<h2 class="text-2xl sm:divider">Why in teams?</h2>
-		<ul class="flex flex-col gap-2">
-			<li>Teamwork is a requirement in the <span class="font-bold">labor market</span>.</li>
-			<li>Encourages mutual <span class="font-bold">support and learning</span>.</li>
-			<li>
-				Promotes <span class="font-bold">creativity</span> which comes from the comparison of different
-				ideas.
-			</li>
-			<li>
-				Students will be more willing to participate in similar competitions
-				<span class="font-bold">(IOI, ICPC)</span>.
-			</li>
-		</ul>
-		<h2 class="text-2xl sm:divider">How is the competition structured?</h2>
-		<ul class="flex flex-col gap-2 ">
-			<li>4 <span class="font-bold">online</span> contests (suggested)</li>
-			<li>1 <span class="font-bold">onsite</span> national final contest</li>
-			<li>1 <span class="font-bold">onsite</span> international contest</li>
-			<li><span class="font-bold">English</span> problem statements for everybody</li>
-			<li>
-				<span class="font-bold">2 PCs</span> per team to solve <span class="font-bold" /> in
-				<span class="font-bold">3 hours</span> (4 at the international contest)
-			</li>
-		</ul>
-		<h2 class="text-2xl sm:divider">How does it work?</h2>
-		<ul class="flex flex-col gap-2 text-justify ">
-			<li>
-				A <span class="font-bold">leader school or organization</span> for each country coordinates the
-				competition: please contact us to become one!
-			</li>
-			<li><span class="font-bold">No costs</span> are required to join the project.</li>
-			<li>
-				The international scientific committee provides <span class="font-bold">tasks</span> and
-				<span class="font-bold">technical support</span>.
-			</li>
-		</ul>
-
-		<div class="sm:divider sm:mt-16">
-			<a
-			href="{base}/registration"
-			class="btn btn-block sm:btn-wide btn-lg btn-primary mt-10 sm:mt-0 self-center"
-		>
-			Registration!
-		</a>
+			<p>
+				For more information see the IIOT website: <a
+					href="https://iio.team/"
+					class="btn-link link-secondary">https://iio.team/</a
+				>
+			</p>
 		</div>
 	</div>
 </Content>
