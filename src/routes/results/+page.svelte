0ba708fac7c82c2e49c5e79d55d8a3ce934f800a
TkcsHnr <tkcshnr@gmail.com>
2025-04-06 21:12:19 +0200

    results page

---
+++ src/routes/results/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Content from "$lib/components/page/Content.svelte";
+	import Hero from "$lib/components/page/Hero.svelte";
+
+</script>
+
+<svelte:head>
+    <title>Results</title>
+</svelte:head>
+
+<Hero>
+    Results
+</Hero>
+<Content>
+    <p class="mx-auto">The results will be published after the contest.</p>
+</Content>
