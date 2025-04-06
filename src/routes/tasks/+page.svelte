a46c22d5ac21ea31ef3d414e1132a26c7e1dd6b1
TkcsHnr <tkcshnr@gmail.com>
2025-04-06 21:13:05 +0200

    tasks page

---
+++ src/routes/tasks/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Content from "$lib/components/page/Content.svelte";
+	import Hero from "$lib/components/page/Hero.svelte";
+
+</script>
+
+<svelte:head>
+    <title>Tasks</title>
+</svelte:head>
+
+<Hero>
+    Tasks
+</Hero>
+<Content>
+    <p class="mx-auto">The tasks will be published after the contest.</p>
+</Content>
