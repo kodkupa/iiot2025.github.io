5f2c6a93eac653fa47dd06f133ac3a2bcf4888cc
Laszlo Nikhazy <laszlo.nikhazy@gmail.com>
2025-04-07 16:01:38 +0200

    Updates

--- _app/immutable/chunks/paths.718de79a.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_jouxo7)==null?void 0:s.base)??"/iiot2025.github.io";var o;const a=((o=globalThis.__sveltekit_jouxo7)==null?void 0:o.assets)??t;export{a,t as b};
