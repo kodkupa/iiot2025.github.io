dfaceb3c901fb737b7ad6b770967e5c5b5036a9a
TkcsHnr <tkcshnr@gmail.com>
2025-05-09 00:53:11 +0200

    Updates

--- _app/immutable/chunks/paths.8b9bf53e.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_tcqe42)==null?void 0:s.base)??"/iiot2025.github.io";var t;const a=((t=globalThis.__sveltekit_tcqe42)==null?void 0:t.assets)??e;export{a,e as b};
