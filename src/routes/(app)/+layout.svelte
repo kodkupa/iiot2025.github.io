dedb62801e369a09f3093a3f29babf93a9e0b368
Hunor Takacs <tkcshnr@gmail.com>
2025-05-23 15:25:58 +0200

    privacy policy pdf update

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -29,8 +29,9 @@
 				<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" class="fill-white"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg>
 			</a>
 		</div>
-		<div>
+		<div class="flex flex-wrap gap-1">
 			<p class="opacity-90">Copyright © {year} - IIOT</p>
+			<a href="{base}/documents/privacy_policy.pdf" class="btn-link link-secondary">Privacy Policy</a>
 		</div>
 	</footer>
 </div>
