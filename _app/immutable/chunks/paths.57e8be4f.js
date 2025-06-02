5cbec02de884ff498e9e61118f09353d1cf42df8
Hunor Takacs <tkcshnr@gmail.com>
2025-06-02 15:20:07 +0200

    Updates

--- _app/immutable/chunks/paths.57e8be4f.js
+++
@@ -1,1 +0,0 @@
-var s;const i=((s=globalThis.__sveltekit_qkibrw)==null?void 0:s.base)??"/iiot2025.github.io";var t;const a=((t=globalThis.__sveltekit_qkibrw)==null?void 0:t.assets)??i;export{a,i as b};
